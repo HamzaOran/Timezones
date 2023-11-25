1ae3722fc8cab7c6f474957eae096a97aeb9a92d
HamzaOran <60844909+HamzaOran@users.noreply.github.com>
2023-11-25 15:02:58 +0300

    export cities

--- src/project/ClockDashboard.jsx
+++ src/project/ClockDashboard.jsx
@@ -1,19 +1,8 @@
 import React from 'react';
 import CityTime from './CityTime';
+import { cities } from './cities';
 
 export default function ClockDashboard() {
-  const cities = [
-    { name: 'New York', timezone: 'America/New_York' },
-    { name: 'LosAngeles', timezone: 'America/Los_Angeles' },
-    { name: 'Chicago', timezone: 'America/Chicago' },
-    { name: 'Denver', timezone: 'America/Denver' },
-    { name: 'London', timezone: 'Europe/London' },
-    { name: 'Paris', timezone: 'Europe/Paris' },
-    { name: 'Berlin', timezone: 'Europe/Berlin' },
-    { name: 'Tokyo', timezone: 'Asia/Tokyo' },
-    { name: 'Sydney', timezone: 'Australia/Sydney' },
-    { name: 'Beijing', timezone: 'Asia/Shanghai' },
-  ];
   return (
     <>
       <h1 className="clock-dashboard">ClockDashboard</h1>
---
+++ src/project/cities.jsx
@@ -0,0 +1,12 @@
+export const cities = [
+  { name: 'New York', timezone: 'America/New_York' },
+  { name: 'LosAngeles', timezone: 'America/Los_Angeles' },
+  { name: 'Chicago', timezone: 'America/Chicago' },
+  { name: 'Denver', timezone: 'America/Denver' },
+  { name: 'London', timezone: 'Europe/London' },
+  { name: 'Paris', timezone: 'Europe/Paris' },
+  { name: 'Berlin', timezone: 'Europe/Berlin' },
+  { name: 'Tokyo', timezone: 'Asia/Tokyo' },
+  { name: 'Sydney', timezone: 'Australia/Sydney' },
+  { name: 'Beijing', timezone: 'Asia/Shanghai' },
+];
